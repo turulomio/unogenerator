18d0bb963d35b8c89134c36bb648cd8ae9665498
turulomio <turulomio@yahoo.es>
2022-03-08 20:37:58 +0100

    addListOfRowsWithStyle is now almost instant

--- benchmark.py
+++ benchmark.py
@@ -1,28 +1,45 @@
-from datetime import datetime
-from os import path
+from datetime import datetime, timedelta, date
+from decimal import Decimal
 from unogenerator import ODS_Standard
 from unogenerator.commons import Coord, ColorsNamed
-from sys import exit
+from unogenerator.reusing.percentage import Percentage
+from unogenerator.reusing.currency import Currency
 
-number=5000
+
+number=10000
+lor=[]
+for i in range(number):
+    lor.append([
+        i, 
+        Coord("B2").addRow(i).string(), 
+        date.today()+timedelta(days=i) , 
+        datetime.now()+timedelta(hours=i), 
+        1.123*i, 
+        Decimal(1.123*i), 
+        Percentage(1, 1.123*i), 
+        Currency(1.123*i, "EUR"), 
+        bool(i % 2), 
+    ])
+headers=["N", "String", "Date", "Datetime", "Float", "Decimal", "Percentage", "Currency", "Boolean"]
+
 
 start=datetime.now()
 doc=ODS_Standard()
 doc.createSheet("Benchmark")
-doc.addCellWithStyle("A1", "N", ColorsNamed.Orange, "BoldCenter")
-for i in range(number):
-    doc.addCellWithStyle(Coord("A2").addRow(i), i)
-print(f"Unogenerator creates {number} cells guessing styles in {datetime.now()-start}")
+doc.setColumnsWidth([4]*20)
+doc.addRowWithStyle("A1", headers, ColorsNamed.Orange, "BoldCenter")
+doc.addListOfRowsWithStyle(Coord("A2"), lor, cellbycell=True)
+print(f"Unogenerator creates {number} cells (cellbycell) in {datetime.now()-start}")
 doc.save("benchmark_unogenerator_guessing_styles.ods")
 doc.close()
 
-
 start=datetime.now()
 doc=ODS_Standard()
 doc.createSheet("Benchmark")
-doc.addCellWithStyle("A1", "N", ColorsNamed.Orange, "BoldCenter")
-for i in range(number):
-    doc.addCellWithStyle(Coord("A2").addRow(i), i, ColorsNamed.White, "Integer")
-print(f"Unogenerator creates {number} cells defining styles in {datetime.now()-start}")
-doc.save("benchmark_unogenerator_defining_styles.ods")
+doc.setColumnsWidth([4]*20)
+doc.addRowWithStyle("A1", headers, ColorsNamed.Orange, "BoldCenter")
+
+doc.addListOfRowsWithStyle(Coord("A2"), lor)
+print(f"Unogenerator creates {number} cells in {datetime.now()-start}")
+doc.save("benchmark_unogenerator_using_massive.ods")
 doc.close()
