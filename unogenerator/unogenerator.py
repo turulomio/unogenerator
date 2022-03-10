1d3953a87108beda0587dffda27ec6ae24b92cb0
turulomio <turulomio@yahoo.es>
2022-03-10 08:42:13 +0100

    Added active sheet debug information

--- unogenerator/unogenerator.py
+++ unogenerator/unogenerator.py
@@ -441,6 +441,7 @@ class ODS(ODF):
     def setActiveSheet(self,  index):
         self.sheet_index=index
         self.sheet=self.document.getSheets().getByIndex(index)
+        debug(f"Sheet '{self.sheet.Name}' ({self.sheet_index}) is now active")
         return self.sheet
     
     ## l measures are in cm can be float
