0039c6123c084e3f1c4a29d04e0a1db56b95dbf5
turulomio <turulomio@yahoo.es>
2022-03-16 05:55:13 +0100

    Added Coord, Range, ColorsNamed to unogenerator import

--- unogenerator/__init__.py
+++ unogenerator/__init__.py
@@ -1,4 +1,4 @@
 ## @package unogenerator.objects
 ## @brief Objects for unogenerator package
 from .unogenerator import ODT, ODS, ODT_Standard, ODS_Standard
-from .commons import __version__, __versiondate__,  __versiondatetime__
+from .commons import __version__, __versiondate__,  __versiondatetime__,  Coord,  Range,  ColorsNamed
