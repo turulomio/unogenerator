1e833e597c577d3b9e9c80539c2ce9ecbd53d0a7
turulomio <turulomio@yahoo.es>
2023-11-05 20:47:47 +0100

    Total coverage: 74%

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,4 +1,5 @@
-from unogenerator.demo import main, main_concurrent
+from unogenerator.demo import main, main_concurrent, demo_ods_standard, demo_odt_standard
+from unogenerator import commons
 
 
 def test_demo():
@@ -9,3 +10,10 @@ def test_demo_concurrent():
     main_concurrent(['--create', '--loops', '1' ])
     main_concurrent(['--remove', '--loops', '1' ])
     assert True
+
+def test_demo_ods_standard():
+    num_instances, first_port=commons.get_from_process_numinstances_and_firstport()
+    demo_ods_standard("es", first_port)
+def test_demo_odt_standard():
+    num_instances, first_port=commons.get_from_process_numinstances_and_firstport()
+    demo_odt_standard("es", first_port)
