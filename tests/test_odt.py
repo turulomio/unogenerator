719f578d474c2cae68fcb3750dc7e03eb926edb0
turulomio <turulomio@yahoo.es>
2023-11-05 19:13:24 +0100

    Addapted metadata to pytest

---
+++ tests/test_odt.py
@@ -0,0 +1,20 @@
+from os import remove
+from unogenerator import ODT_Standard, ODT
+
+
+def test_metadata():
+    with ODT_Standard() as doc:
+        doc.setMetadata(
+            title="Metadata example",
+            subject="UnoGenerator testing",
+            author="Turulomio",
+            description="This testing works with metadata methods",
+            keywords=["Metadata", "Testing"],
+        )
+        doc.save("delete_metadata.odt")
+
+    with ODT("delete_metadata.odt") as doc:
+        print(doc.getMetadata())
+        assert doc.getMetadata()["Title"]== "Metadata example"
+        
+    remove("delete_metadata.odt")
